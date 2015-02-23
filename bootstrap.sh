2ab35092ae730bddb8f52217df63af38875f3112
fahhrad <ward.coessens@gmail.com>
2015-02-23 22:56:58 +0100

    adding gitignore. Adding export of BLASTDB to .bashrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,15 +9,16 @@ deb mirror://mirrors.ubuntu.com/mirrors.txt precise-security main restricted uni
 sudo cat mirrors.list /etc/apt/sources.list > /etc/apt/sources.list
 
 sudo apt-get update
-sudo apt-get install -y ncbi-blast+ rabbitmq-server python3 python-biopython
+sudo apt-get install -y ncbi-blast+ rabbitmq-server python3
 
+# Create virtualenv for executing loaders
 
 # Getting test genome
 wget ftp://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/drosoph.nt.gz
 gunzip drosoph.nt.gz
 
 export BLASTDB=/data/blastdb
-
+echo "export BLASTDB=/data/blastdb" >> ~/.bashrc
 
 sudo mkdir -p /data/blastdb/drosoph
 sudo mv drosoph.nt /data/blastdb/drosoph
